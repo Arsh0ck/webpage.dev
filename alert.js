710637ef35289594f8bc110d09682d3d1f17572d
Arsh0ck <115183794+Arsh0ck@users.noreply.github.com>
2023-06-27 21:26:26 -0400

    first upload

---
+++ alert.js
@@ -0,0 +1,15 @@
+<script type="">
+    function mostrar(){
+
+    }
+</script>
+
+
+<!--
+	<!DOCTYPE HTML>
+<html lang="en-US">
+<head>
+	<script>alert("Gracias por contactar conmigo. Te responderé lo más rápido posible".);</script>
+	<meta HTTP-EQUIV="REFRESH" content="0; url=http://www.arturocarrillo.cl">
+</head>
+>
